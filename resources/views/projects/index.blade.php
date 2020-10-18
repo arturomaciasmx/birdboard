9d8ca7b6eadeb4f905f2d22ced39da8252d545e2
Arturo Macias <arturomaciasmx@gmail.com>
2020-10-18 15:21:33 -0700

    Go Go Gadget Tailwind

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,16 +9,24 @@
     </x-slot>
 
     <div class="container mx-auto py-8">
-        <ul>
+
+        <div class="projects-container flex flex-wrap">
             @forelse ($projects as $project)
-                <li>
-                    <a href="{{ $project->path() }}" class="hover:text-blue-500">
-                        {{ $project->title }}</li>
-                    </a>
+            <div class="lg:w-1/3 md:w-1/2 w-full p-4">
+                <div class="rounded shadow bg-white py-4 h-48">
+                    <h3 class="mb-4 border-l-4 border-solid border-blue-500 px-4">{{ $project->title }}</h3>
+                    <div class="px-4">
+                        <p class="text-gray-500">
+                            {{ Str::limit($project->description, 300) }}
+                        </p>
+                    </div>
+                </div>
+            </div>
             @empty
                 <li>No projects jet</li>
             @endforelse
-        </ul>
+        </div> {{-- project container --}}
+
     </div>
 
 </x-app-layout>
